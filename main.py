cfd4e0d105f4784b6a629b1ef09a488dc9bf8059
Amirmahdi Namjoo <amirm137878@gmail.com>
2021-04-16 04:32:37 +0430

    Add documentation for find_score_in_quera

--- main.py
+++ main.py
@@ -6,12 +6,28 @@ df: pd.DataFrame = pd.read_csv("1.csv")
 ERROR_CODE_MAGIC_NUMBER = -113543635432
 
 
-def find_score_in_quera(std_number, judge_score_string="judge score: ", folder_structure="scores",
+def find_score_in_quera(student_number, judge_score_string="judge score:", folder_structure_path="scores",
                         file_name="result.txt"):
+    """
+    This function tries to open file in the address "./folder_structure_path/std_number/file_name"
+    and tries to find an string like "judge_score_string score" in the text. If it find the score, it will return it
+    otherwise it will return an ErrorCode which is defined as ERROR_CODE_MAGIC_NUMBER.
+
+    :param student_number: int or str
+        Student Number. The main identifier in folder structure
+    :param judge_score_string: str
+        Instructs the code to find an string like "judge_score_string (/d+)" in the file. (/d+) is a regex for numbers.
+    :param folder_structure_path: str
+        Path to the folder in which we can find folders of each student's results.
+    :param file_name: str
+        Final file name. For Quera it is usually result.txt
+    :return: int or str
+        Score of specified student or ERROR_CODE_MAGIC_NUMBER if it cannot find the file.
+    """
     try:
-        file = open(f"{folder_structure}/{std_number}/{file_name}", mode='r', encoding="utf8")
+        file = open(f"{folder_structure_path}/{student_number}/{file_name}", mode='r', encoding="utf8")
         result = file.read()
-        retval = re.search(f"{judge_score_string}(\\d+)", result).group(1)
+        retval = re.search(f"{judge_score_string} (\\d+)", result).group(1)
         file.close()
         return retval
     except FileNotFoundError:
@@ -23,7 +39,7 @@ def put_scores_in_dataframe(src_csv: str, std_id_header="Students", score_headin
                             quera_file_name="result.txt"):
     score_df: pd.DataFrame = pd.read_csv(src_csv)
     for std in score_df[std_id_header]:
-        score = find_score_in_quera(std, judge_score_string=judge_score_string, folder_structure=folder_structure,
+        score = find_score_in_quera(std, judge_score_string=judge_score_string, folder_structure_path=folder_structure,
                                     file_name=quera_file_name)
         if score==ERROR_CODE_MAGIC_NUMBER:
             score_df.loc[score_df[std_id_header] == std, score_heading] = not_found_score
