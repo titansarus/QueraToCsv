60a61e41dbe8a8db123c6c49c63a71bc003b7a70
Amirmahdi Namjoo <amirm137878@gmail.com>
2021-04-17 02:09:45 +0430

    Add documentation for put_scores_in_dataframe

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def english_to_persian_number(number):
     in their student number and for each student number, we must check both English and Persian alphabet.
 
     :param number: int or str
-     Number in English alphabet
+        Number in English alphabet
     :return: str
-     Number in Persian alphabet
+        Number in Persian alphabet
     """
     number = str(number)
     input_chars = '1234567890'
@@ -24,26 +24,26 @@ def english_to_persian_number(number):
     return number.translate(translation_table)
 
 
-def find_score_in_quera(student_number, judge_score_string="judge score:", folder_structure_path="scores",
-                        file_name="result.txt"):
+def find_score_in_quera(student_id, judge_score_string="judge score:", folder_structure_path="scores",
+                        result_filename="result.txt"):
     """
-    This function tries to open file in the address "./folder_structure_path/std_number/file_name"
+    This function tries to open file in the address "./folder_structure_path/student_id/file_name"
     and tries to find an string like "judge_score_string score" in the text. If it find the score, it will return it
     otherwise it will return an ErrorCode which is defined as ERROR_CODE_MAGIC_NUMBER.
 
-    :param student_number: int or str
-        Student Number. The main identifier in folder structure
+    :param student_id: int or str
+        Student id. The main identifier in folder structure
     :param judge_score_string: str
         Instructs the code to find an string like "judge_score_string (/d+)" in the file. (/d+) is a regex for numbers.
     :param folder_structure_path: str
         Path to the folder in which we can find folders of each student's results.
-    :param file_name: str
+    :param result_filename: str
         Final file name. For Quera it is usually result.txt
     :return: int or str
         Score of specified student or ERROR_CODE_MAGIC_NUMBER if it cannot find the file.
     """
-    english_path = f"{folder_structure_path}/{student_number}/{file_name}"
-    persian_path = f"{folder_structure_path}/{english_to_persian_number(student_number)}/{file_name}"
+    english_path = f"{folder_structure_path}/{student_id}/{result_filename}"
+    persian_path = f"{folder_structure_path}/{english_to_persian_number(student_id)}/{result_filename}"
     if os.path.exists(english_path):
         file = open(english_path, encoding='utf8')
     elif os.path.exists(persian_path):
@@ -56,20 +56,46 @@ def find_score_in_quera(student_number, judge_score_string="judge score:", folde
     return retval
 
 
-def put_scores_in_dataframe(src_csv: str, std_id_header="Students", score_heading="Score", not_found_score="0",
-                            judge_score_string="judge score: ", folder_structure="scores",
-                            quera_file_name="result.txt"):
+def put_scores_in_dataframe(src_csv: str, student_id_heading="Students", score_heading="Score", not_found_score="0",
+                            judge_score_string="judge score: ", folder_structure_path="scores",
+                            result_filename="result.txt"):
+    """
+    Put scores that are in Quera results files into a Pandas DataFrame.
+    At first, it Loads a base csv located in `src_csv` that includes student IDs and headers for Student column and Score column.
+    Then it searches the directories in `folder_structure_path` for that student IDs and get their scores.
+    After that it puts them in a DataFrame and returns that.
+
+
+    :param src_csv: str
+        Name of the Source CSV file that has student IDs and header name for Students and Scores.
+    :param student_id_heading: str
+        Name of the heading in CSV file that has Student IDs.
+    :param score_heading: str
+        Name of the heading in CSV file that is going to have Scores.
+    :param not_found_score: int
+        The default score for students that didn't submitted their answer of their result file couldn't be found.
+    :param judge_score_string: str
+        Instructs the code to find an string like "judge_score_string (/d+)" in the file. (/d+) is a regex for numbers.
+    :param folder_structure_path: str
+        Path to the folder in which we can find folders of each student's results.
+    :param result_filename: str
+        Final file name. For Quera it is usually result.txt
+    :return: Pandas.DataFrame
+        A Pandas DataFrame that contains student IDs and scores.
+    """
     score_df: pd.DataFrame = pd.read_csv(src_csv)
-    for std in score_df[std_id_header]:
-        score = find_score_in_quera(std, judge_score_string=judge_score_string, folder_structure_path=folder_structure,
-                                    file_name=quera_file_name)
+    for std in score_df[student_id_heading]:
+        score = find_score_in_quera(std, judge_score_string=judge_score_string,
+                                    folder_structure_path=folder_structure_path,
+                                    result_filename=result_filename)
         if score == ERROR_CODE_MAGIC_NUMBER:
-            score_df.loc[score_df[std_id_header] == std, score_heading] = not_found_score
+            score_df.loc[score_df[student_id_heading] == std, score_heading] = not_found_score
         else:
-            score_df.loc[score_df[std_id_header] == std, score_heading] = score
+            score_df.loc[score_df[student_id_heading] == std, score_heading] = score
 
     return score_df
 
 
-score_df = put_scores_in_dataframe("1.csv")
-score_df.to_csv("result.csv")
+if __name__ == '__main__':
+    score_df = put_scores_in_dataframe("1.csv")
+    score_df.to_csv("result.csv")
